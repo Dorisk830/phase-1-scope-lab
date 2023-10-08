19a88243d74daf14e7961662dd2c8a9fcafa94c6
DorisK830 <doris.kiprop@student.moringaschool.com>
2023-10-08 17:36:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob"; 
+function upperCaseCustomerName() {
+    if (customerName) {
+        return customerName = customerName.toUpperCase();
+    }
+}
+function setBestCustomer(){
+    return bestCustomer= "not bob"
+    
+}
+function overwriteBestCustomer (){
+    return bestCustomer= "maybe bob"
+}
+const leastFavoriteCustomer= "doris"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="Jep"
+
+}
+
+
+
+
